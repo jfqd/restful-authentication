fbce2fd613452bccdc7e07218691a304702715c2
Jeremy Durham <jeremydurham@gmail.com>
2009-10-21 23:16:23 -0400

    Added a begin/rescue to the ra_env and a helpful message about moving include AuthenticatedSystem to ApplicationController

--- generators/authenticated/templates/features/step_definitions/ra_env.rb
+++ generators/authenticated/templates/features/step_definitions/ra_env.rb
@@ -1,1 +1,7 @@
-ApplicationController.send(:public, :logged_in?, :current_<%= file_name %>, :authorized?)
+begin
+  ApplicationController.send(:public, :logged_in?, :current_<%= file_name %>, :authorized?)
+rescue NameError => e
+  puts "An exception occurred: #{e}"
+  puts 'Did you forget to move "include AuthenticatedSystem" from sessions_controller.rb to application_controller.rb?'
+  exit!
+end
