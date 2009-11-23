ec112e8f982e199b9786f794e71098ddd354ee42
Jeremy Durham <jeremydurham@gmail.com>
2009-11-23 17:12:38 -0500

    Added requiring stateful and aasm roles

--- lib/restful_authentication.rb
+++ lib/restful_authentication.rb
@@ -1,3 +1,6 @@
 require File.join(File.dirname(__FILE__), "..", "lib", "authentication")
 require File.join(File.dirname(__FILE__), "..", "lib", "authentication", "by_password")
 require File.join(File.dirname(__FILE__), "..", "lib", "authentication", "by_cookie_token")
+require File.join(File.dirname(__FILE__), "..", "lib", "authorization")
+require File.join(File.dirname(__FILE__), "..", "lib", "authorization", "aasm_roles")
+require File.join(File.dirname(__FILE__), "..", "lib", "authentication", "stateful_roles")
