02ab3c198a0df06a4f7739852350ff0b5a78aee9
Jeremy Durham <jeremydurham@gmail.com>
2009-11-30 01:24:26 -0500

    Cleaned up formatting and added nil for activation_code

--- generators/authenticated/templates/spec/blueprints/user.rb
+++ generators/authenticated/templates/spec/blueprints/user.rb
@@ -3,11 +3,11 @@
   password 'testing'
   password_confirmation { password }
   email { Faker::Internet.email }
-  <% if options[:include_activation] %>
-    activation_code
-    activated_at { 5.days.ago }    
-  <% end %>
-  <% if options[:stateful] %>
-    state { 'active' }
-  <% end %>
+<% if options[:include_activation] -%>
+  activation_code { nil }
+  activated_at { 5.days.ago }
+<% end -%>
+<% if options[:stateful] -%>
+  state { 'active' }
+<% end -%>
 end
