703bdba52f92d6c2c556b2a58bc7263988f13452
jfqd <jfqd@blun.org>
2015-03-20 22:58:48 +0100

    make it work with unicode and ruby 1.9; localize error messages

--- lib/authentication.rb
+++ lib/authentication.rb
@@ -1,22 +1,23 @@
+# encoding: utf-8
 module Authentication
   mattr_accessor :login_regex, :bad_login_message, 
     :name_regex, :bad_name_message,
     :email_name_regex, :domain_head_regex, :domain_tld_regex, :email_regex, :bad_email_message
 
-  self.login_regex       = /\A\w[\w\.\-_@]+\z/                     # ASCII, strict
-  # self.login_regex       = /\A[[:alnum:]][[:alnum:]\.\-_@]+\z/     # Unicode, strict
+  # self.login_regex       = /\A\w[\w\.\-_@]+\z/                     # ASCII, strict
+  self.login_regex       = /\A[[:alnum:]][[:alnum:]\.\-_@]+\z/     # Unicode, strict
   # self.login_regex       = /\A[^[:cntrl:]\\<>\/&]*\z/              # Unicode, permissive
 
-  self.bad_login_message = "use only letters, numbers, and .-_@ please.".freeze
+  self.bad_login_message = lambda { I18n.t('authentication.errors.bad_login_message') }
 
   self.name_regex        = /\A[^[:cntrl:]\\<>\/&]*\z/              # Unicode, permissive
-  self.bad_name_message  = "avoid non-printing characters and \\&gt;&lt;&amp;/ please.".freeze
+  self.bad_name_message  = lambda { I18n.t('authentication.errors.bad_name_message') }
 
-  self.email_name_regex  = '[\w\.%\+\-]+'.freeze
-  self.domain_head_regex = '(?:[A-Z0-9\-]+\.)+'.freeze
-  self.domain_tld_regex  = '(?:[A-Z]{2}|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|jobs|museum)'.freeze
-  self.email_regex       = /\A#{email_name_regex}@#{domain_head_regex}#{domain_tld_regex}\z/i
-  self.bad_email_message = "should look like an email address.".freeze
+  self.email_name_regex  = '[A-Za-z0-9\-\+\_]+'.freeze
+  self.domain_head_regex = '(\.[A-Za-z0-9\-\+\_]+)*'.freeze
+  self.domain_tld_regex  = '(([a-z0-9]([-a-z0-9]*[a-z0-9])?\.){1,4})([a-z]{2,15})'.freeze
+  self.email_regex       = /#{email_name_regex}\@#{domain_head_regex}#{domain_tld_regex}/i
+  self.bad_email_message = lambda { I18n.t('authentication.errors.bad_email_message') }
 
   def self.included(recipient)
     recipient.extend(ModelClassMethods)
